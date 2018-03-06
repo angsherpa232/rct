43d8ab0e0bc73175fed5ca7bbb771e8f009515ad
angsherpa232 <angsherpa232@gmail.com>
2018-03-07 00:42:51 +0100

    digital clock

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor (){
     super();
-    var operation;
-    this.state = {
-      showMe: true
+    var currentTime;
+    this.state={
+      time: new Date()
     }
   }
 
-operation=()=>this.setState({
-  showMe: !this.state.showMe
-})
+  currentTime = ()=>
+    this.setState({
+      time: new Date()
+    })
+
+  componentWillMount ()
+  {
+    setInterval(()=>this.currentTime(),1000)
+  }
 
     render() {
     return (
       <div>
-      <h1>
-      Show and hide Elements
-      </h1>
-      {
-        this.state.showMe?
-        <div>
-        Please hide me
-        </div>
-        :null
-      }
-
-      <button onClick={()=>this.operation()}>Click me</button>
-      </div>
+    <h1>
+    <span>The curret Time is:</span> <br/>
+    {this.state.time.toLocaleTimeString()}
+    </h1>
+    </div>
     );
   }
 };
