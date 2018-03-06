0265b34b7c4e7e603f36e3d7bfd388f3ee06fb1d
angsherpa232 <angsherpa232@gmail.com>
2018-03-06 16:27:43 +0100

    event handling with arrow function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-//fun is a default function, the name can be any random name
-//funcions other than default needs to be in {} as shown below
-import fun, {other} from './fun';
 class App extends Component {
-  constructor ()
-  {
-    super ();
-    //fun() is a default function
-    //other() is not default function whose name need to be exact as the function itself
-    fun();
-    other();
-    this.state = {
-      data: [],
-    }
+  constructor (){
+    super();
+    var callMe;
   }
 
-
-  render() {
+  callMe = () => alert("Hello world");
+    render() {
     return (
-      <div>
+      <div className="App">
+      <h1>React event handling</h1>
+      <button onClick={()=>this.callMe()}>Click me</button>
       </div>
     );
   }
