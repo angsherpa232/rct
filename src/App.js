78cffc95d0b025125b7764458ee2aeee47da40b3
angsherpa232 <angsherpa232@gmail.com>
2018-03-06 17:15:17 +0100

    map function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 class App extends Component {
-
-//fetching is throwing error
-  componentWillMount (){
-    fetch('./request.php')
-    .then((response)=>response.json())
-    .then((responseJson)=>
-    {
-    console.log(responseJson)
-    })
+  constructor (){
+    super();
+    this.state = {
+      data: [
+        {
+          name: "john",
+          email: "john@gmail.com"
+        },
+        {
+          name: "kevin",
+          email: "kevin@gmail.com"
+        },
+        {
+          name: "sam",
+          email: "sam@gmail.com"
+        },
+      ]
+    }
   }
-
     render() {
     return (
       <div className="App">
-      <h1>Call php web service</h1>
+      <h1>Map function</h1>
+      <h1>Map function in react</h1>
+      {
+        this.state.data.map((rowdata, key)=>
+        <div>
+        <span>{key} </span>
+        <span>{rowdata.name} </span><br/>
+        <span> {"  "}{rowdata.email} </span>
+        </div>
+      )
+      }
       </div>
     );
   }
